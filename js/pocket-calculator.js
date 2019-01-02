b714d7503eaac6a1c7932567f0c06b9e3a2da278
first Last <tanyamaini@hotmail.com>
2019-01-02 13:05:23 -0500

    update

--- js/pocket-calculator.js
+++ js/pocket-calculator.js
@@ -1,92 +1,95 @@
-
-let output = "";
-
-function button0(){
-  let p = document.getElementById("output");
-  output = output + 0;
-  p.innerHTML = output;
+function button() {
+  document.calculator.display.value = document.calculator.display.value + num
 }
 
-function button1(){
-  let p = document.getElementById("output");
-  output = output + 1;
-  p.innerHTML = output;
-}
-
-function button2(){
-  let p = document.getElementById("output");
-  output = output + 2;
-  p.innerHTML = output;
-}
-
-function button3(){
-  let p = document.getElementById("output");
-  output = output + 3;
-  p.innerHTML = output;
-}
-
-function button4(){
-  let p = document.getElementById("output");
-  output = output + 4;
-  p.innerHTML = output;
-}
-
-function button5(){
-  let p = document.getElementById("output");
-  output = output + 5;
-  p.innerHTML = output;
-}
-
-function button6(){
-  let p = document.getElementById("output");
-  output = output + 6;
-  p.innerHTML = output;
-}
-
-function button7(){
-  let p = document.getElementById("output");
-  output = output + 7;
-  p.innerHTML = output;
-}
-
-function button8(){
-  let p = document.getElementById("output");
-  output = output + 8;
-  p.innerHTML = output;
-}
-
-function button9(){
-  let p = document.getElementById("output");
-  output = output + 9;
-  p.innerHTML = output;
-}
-
-function buttonAdd(){
-  let p = document.getElementById("output");
-  output = output + "+";
-  p.innerHTML = output;
-}
-
-function buttonSubtract(){
-  let p = document.getElementById("output");
-  output = output + "-";
-  p.innerHTML = output;
-}
-
-function buttonMultiply(){
-  let p = document.getElementById("output");
-  output = output + "x";
-  p.innerHTML = output;
-}
-
-function buttonDivide(){
-  let p = document.getElementById("output");
-  output = output + "/";
-  p.innerHTML = output;
-}
-
-function buttonDecimal(){
-  let p = document.getElementById("output");
-  output = output+".";
-  p.innerHTML = output;
-}
+// let output = "";
+//
+// function button0(){
+//   let p = document.getElementById("output");
+//   output = output + 0;
+//   p.innerHTML = output;
+// }
+//
+// function button1(){
+//   let p = document.getElementById("output");
+//   output = output + 1;
+//   p.innerHTML = output;
+// }
+//
+// function button2(){
+//   let p = document.getElementById("output");
+//   output = output + 2;
+//   p.innerHTML = output;
+// }
+//
+// function button3(){
+//   let p = document.getElementById("output");
+//   output = output + 3;
+//   p.innerHTML = output;
+// }
+//
+// function button4(){
+//   let p = document.getElementById("output");
+//   output = output + 4;
+//   p.innerHTML = output;
+// }
+//
+// function button5(){
+//   let p = document.getElementById("output");
+//   output = output + 5;
+//   p.innerHTML = output;
+// }
+//
+// function button6(){
+//   let p = document.getElementById("output");
+//   output = output + 6;
+//   p.innerHTML = output;
+// }
+//
+// function button7(){
+//   let p = document.getElementById("output");
+//   output = output + 7;
+//   p.innerHTML = output;
+// }
+//
+// function button8(){
+//   let p = document.getElementById("output");
+//   output = output + 8;
+//   p.innerHTML = output;
+// }
+//
+// function button9(){
+//   let p = document.getElementById("output");
+//   output = output + 9;
+//   p.innerHTML = output;
+// }
+//
+// function buttonAdd(){
+//   let p = document.getElementById("output");
+//   output = output + "+";
+//   p.innerHTML = output;
+// }
+//
+// function buttonSubtract(){
+//   let p = document.getElementById("output");
+//   output = output + "-";
+//   p.innerHTML = output;
+// }
+//
+// function buttonMultiply(){
+//   let p = document.getElementById("output");
+//   output = output + "x";
+//   p.innerHTML = output;
+// }
+//
+// function buttonDivide(){
+//   let p = document.getElementById("output");
+//   output = output + "/";
+//   p.innerHTML = output;
+// }
+//
+// function buttonDecimal(){
+//   let p = document.getElementById("output");
+//   output = output+".";
+//   p.innerHTML = output;
+// }
